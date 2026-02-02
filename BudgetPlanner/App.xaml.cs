aa704bc22e4e1a50503118171a1b1e82e38cb23e
Eppu Jauhojarvi <eppu.jauhojarvi@gmail.com>
2026-02-02 19:26:42 +0100

    Add project files.

---
+++ BudgetPlanner/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace BudgetPlanner
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
